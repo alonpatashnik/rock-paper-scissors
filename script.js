d72221b8fe3763e20874fe555c9ab02960a7fcbc
alonpatashnik <alonp714@gmail.com>
2022-06-24 10:28:22 -0700

    Added html file and javascript file and added pseudo-code

---
+++ script.js
@@ -0,0 +1,24 @@
+//TODO: randomly choose computer choice
+    //TODO: randomly select from  r, p, or s (maybe an array?)
+    //TODO: store in variable  
+//TODO: collect users choice (R,P,S)
+    //TODO: ask user for r,p,or s via popup
+    //TODO: EDGE CASE: user doenst pick r,p or s 
+//TODO: figure out result
+    //TODO: win: r:s, p:r, s:p
+    //TODO: loss: s:r, r:p, p:s
+    //TODO: tie s:s, r:r, p:p
+//TODO: Log stats
+    //TODO: craete variables for win, loss, and tie
+    //TODO: update appropriate variable based on result
+    //TODO: display to user
+//Keep playing until quit
+var names = ["Joe","Henry","William","Niles"]
+
+// var randomNum  = Math.random()
+// console.log(randomNum)
+// var numTimesLength = randomNum * names.length
+// console.log(numTimesLength)
+// var roundDown = Math.floor(numTimesLength)
+// console.log(roundDown)
+console.log(names[Math.floor(Math.random()*names.length)])
